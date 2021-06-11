5306d8c2aab061222a9c8460f3df6048546dcf2e
nw <55707935+nathanwooten@users.noreply.github.com>
2021-06-11 14:33:19 -0400

    Update FilesystemTraitScan.php

--- src/FilesystemTraitScan.php
+++ src/FilesystemTraitScan.php
@@ -2,31 +2,114 @@
 
 namespace nathanwooten\Filesystem;
 
-trait FilesystemTraitScan
+use nathanwooten\Filesystem\{
+
+	FilesystemFilterDirectory,
+	FilesystemFilterInterface,
+	FilesystemTraitImplode,
+	FilesystemTraitNormalize
+
+};
+
+use Exception;
+use OutOfBoundsException;
+
+class FilesystemHelperScan
 {
 
-	use FilesystemTraitHelper;
+	use FilesystemTraitExplode;
+	use FilesystemTraitImplode;
+	use FilesystemTraitNormalize;
 
 	public function scan( $readable, array $filters = [] ) {
 
-		if ( ! $readable ) {
-			throw new Exception( 'Unreadable provided' );
+		$scan = [];
+
+		if ( is_array( $readable ) ) {
+			$readable = $this->implode( $readable );
 		}
 
-		$scan = $this->act( __TRAIT__, __FUNCTION__, ...func_get_args() );
+		if ( ! is_readable( $readable ) ) {
+
+			throw new Exception( 'Not readable for scan' );
+		}
+
+		foreach ( scandir( $readable ) as $key => $item ) {
+
+			$item = $this->apply( $item, $filters );
+
+			if ( $item ) {
+				$scan[ $key ] = $item;
+			}
+		}
+
 		return $scan;
 
 	}
 
-	public function iterateScan( $readable, array $filters = [] ) {
+	public function apply( $item, array $filters )
+	{
 
-		if ( ! $readable ) {
-			throw new Exception( 'Unreadable provided' );
+		if ( empty( $item ) ) {
+			return;
 		}
 
-		$iterateScan = $this->act( __TRAIT__, __FUNCTION__, ...func_get_args() );
-		return $iterateScan;
+		foreach ( $filters as $filter ) {
+
+			if ( is_string( $filter ) ) {
+				if ( ! class_exists( $filter ) || ! in_array( __NAMESPACE__ . '\\' . 'FilesystemFilterInterface', class_implements( $filter ) ) ) {
+
+					throw new OutOfBoundsException( 'Invalid filter' );
+				}
+				$filter = new $filter;
+			}
+
+			$item = $filter( $item );
+		}
+
+		return $item;
 
 	}
+
+	public function iterateScan(
+		$readable,
+		$filters = [
+			'nathanwooten\Filesystem\FilesystemFilterDirectoryNot',
+			'nathanwooten\Filesystem\FilesystemFilterDots'
+		]
+	) {
+
+		$scan = [];
+
+		$dirs = array_values( $this->scan( $readable, $filters ) );
+
+		$rArray = $this->explode( $readable );
+
+		if ( ! empty( $dirs ) ) {
+			foreach ( $dirs as $key => $dir ) {
+
+				$keys[] = $key;
+
+				$norm = $this->normalize( $readable );
+				if ( is_array( $norm ) ) {
+					$norm = implode( '', $norm );
+				}
+
+				$childReadable = $norm . $dir;
+/*
+				foreach ( $keys as $kee ) {
+
+					$scan &= $dirs[ $kee ];
+				}
+*/
+				$scan = $this->scan( $childReadable, $filters );
+			}
+		}
+
+		$list = $scan;
+		return $list;
+
+	}
+
 
 }
