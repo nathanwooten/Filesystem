b7448f1d8be82abba5e9ccb49cf41d66882efa25
nw <55707935+nathanwooten@users.noreply.github.com>
2021-06-18 16:33:15 -0400

    index now autoloaders instead of requiring

--- src/index.php
+++ src/index.php
@@ -1,1 +1,14 @@
+<?php
 
+$namespace = 'nathanwooten\Filesystem';
+$dir = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
+
+spl_autoload_register( function ( $qualified ) {
+
+  $file = str_replace( $namespace, $dir, $qualified );
+
+  if ( file_exists( $file ) ) {
+    require_once $file;
+  }
+
+} );
