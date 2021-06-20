7d6db2d4c25a3bcd2688c22e53aed9dcb5bdb98a
nw <55707935+nathanwooten@users.noreply.github.com>
2021-06-20 14:14:32 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 $namespace = 'nathanwooten\Filesystem';
 $dir = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
 
-spl_autoload_register( function ( $qualified ) {
+spl_autoload_register( function ( $qualified ) use ( $namespace, $dir ) {
 
   $file = str_replace( $namespace, $dir, $qualified );
 
@@ -12,3 +12,17 @@ spl_autoload_register( function ( $qualified ) {
   }
 
 } );
+
+$fdir . 'functions' . DIRECTORY_SEPARATOR;
+if ( ! is_readable( $fdir ) ) {
+  return false;  
+}
+$scan = scandir( $fdir );
+foreach ( $scan as $item ) {
+  if ( is_file( $item ) ) {
+    require_once $fdir . $item;
+  }
+  
+}
+
+return true;
